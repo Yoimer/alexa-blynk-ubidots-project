0792fcba6fef4441d8c2ae995da043d3fc331271
Yoimer Román <yoimer.roman@technoglobalinc.com>
2020-04-19 10:12:27 -0400

    added gpio 5 configuration for fan

--- dht11_and_servo.py
+++ dht11_and_servo.py
@@ -1,5 +1,5 @@
 # code that prints temperature or humidity when user inputs TEMP or HUMD
-# it also open or close servo when user inputs OPEN or CLOSE
+# it also open or close servo when user inputs OPEN or CLOSE, activates relays depending on inputs such as STARTFAN STOPFAN
 # this is has to be used entering the command: su root
 # password: raspberry
 
@@ -22,12 +22,19 @@ import Adafruit_DHT
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 27
 
+# gpio libraries
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# set pin 5 as output
+GPIO.setup(5,GPIO.OUT)
+
 while True:
     #reads humidity and temperature
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 
-    # prompts user for input
-    user_input = input("Please enter TEMP or HUMD or OPEN or CLOSE: ")
+    user_input = input("Please enter any of the following commands: \n TEMP \n HUMD \n OPEN \n CLOSE \n STARTFAN \n STOPFAN: ")
 
     # if input is HUMD, prints humidity
     if(user_input == "HUMD"):
@@ -56,5 +63,17 @@ while True:
         for pulse in range(220, 100, -1):
             wiringpi.pwmWrite(18, pulse)
             time.sleep(delay_period)
+
+    # if input is STARTFAN, turns on fan
+    elif(user_input == "STARTFAN"):
+        print("Starting Fan")
+        GPIO.output(5,GPIO.HIGH)
+
+    # if input is STOPFAN, turns off fan
+    elif(user_input == "STOPFAN"):
+        print("Stopping Fan")
+        GPIO.output(5,GPIO.LOW)
+
     else:
-        print("Unrecognized input, enter TEMP or HUMD or OPEN or CLOSE: ")
+        print("Unrecognized input")
+        time.sleep(1)
